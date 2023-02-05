c701d2413d3bc1e174d3d2ce538264a3a906273b
Matthias Kovacic <rocketpencil.studios@gmail.com>
2023-02-05 22:30:15 +0100

    Add min/max rules for float/integer

---
+++ delegate-core/src/main/java/com/github/polyrocketmatt/delegate/core/command/argument/rule/FloatMinRule.java
@@ -0,0 +1,42 @@
+package com.github.polyrocketmatt.delegate.core.command.argument.rule;
+
+import com.github.polyrocketmatt.delegate.api.command.argument.CommandArgument;
+import com.github.polyrocketmatt.delegate.api.command.argument.rule.ArgumentRule;
+import com.github.polyrocketmatt.delegate.api.command.argument.rule.ArgumentRuleResult;
+import com.github.polyrocketmatt.delegate.api.command.argument.rule.RuleData;
+
+public class FloatMinRule extends ArgumentRule<Boolean> {
+
+    /**
+     * Creates a new rule that checks if an input is a number and is greater than the specified value.
+     *
+     * @param min The minimum value.
+     * @param inclusive Whether the minimum value is inclusive.
+     */
+    public FloatMinRule(float min, boolean inclusive) {
+        super(input -> {
+            try {
+                float value = Float.parseFloat(input);
+                return new RuleData<>(inclusive ? min <= value : min < value);
+            } catch (NumberFormatException ex) { return new RuleData<>(false); }
+        });
+    }
+
+    /**
+     * Creates a new rule that checks if an input is a number and is greater than the specified value.
+     *
+     * @param min The minimum value.
+     */
+    public FloatMinRule(float min) {
+        this(min, true);
+    }
+
+    @Override
+    public ArgumentRuleResult interpretResult(CommandArgument<?> argument, String input, RuleData<?> output) {
+        if (!(output.value() instanceof Boolean result))
+            return new ArgumentRuleResult(ArgumentRuleResult.Result.FAILURE, "Expected result of rule did not match");
+        if (!result)
+            return new ArgumentRuleResult(ArgumentRuleResult.Result.FAILURE, "Value was null or not a number greater than the minimum");
+        return new ArgumentRuleResult(ArgumentRuleResult.Result.SUCCESS, "Successfully passed %s".formatted(getClass().getSimpleName()));
+    }
+}
