848e181afc80a25cf0210e9923edec906b7d149b
Matthias Kovacic <rocketpencil.studios@gmail.com>
2023-02-20 16:27:42 +0100

    Git rebasing

---
+++ delegate-core/src/test/java/com/github/polyrocketmatt/delegate/core/command/tree/CommandNodeTest.java
@@ -0,0 +1,7 @@
+// Copyright (c) Matthias Kovacic. All rights reserved.
+// Licensed under the MIT license.
+
+package com.github.polyrocketmatt.delegate.core.command.tree;
+
+public class CommandNodeTest {
+}
